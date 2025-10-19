afb6b0b2d695cfab516ca5628a62c91d5f153d35
Sai Mounika <battinasaimounika558@gmail.com>
2025-10-19 20:23:18 +0530

    rate limiter updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 const rateLimit=new Ratelimit({
     redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(5,"20 s")
+    limiter:Ratelimit.slidingWindow(100,"60 s")
 })
 
 export default rateLimit
